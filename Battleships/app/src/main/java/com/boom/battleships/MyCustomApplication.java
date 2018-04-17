ad6f614ef6d72fa11ce45ee7ad3ae45009846bda
Unknown <erick.hdez06@outlook.com>
2018-04-16 22:06:41 -0600

    Crashlytics

--- Battleships/app/src/main/java/com/boom/battleships/MyCustomApplication.java
+++ Battleships/app/src/main/java/com/boom/battleships/MyCustomApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.boom.battleships.asynctasks.APICalls;
 import com.cloudinary.android.MediaManager;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by erickhdez on 27/3/18.
@@ -13,6 +15,7 @@ public class MyCustomApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         MediaManager.init(getApplicationContext());
         APICalls.init(getApplicationContext());
